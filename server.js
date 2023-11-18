3d27f63f763c634c3e0afa851dd128deb7c74e35
pavarun.p <pavarun.p@houseofdev.tech>
2023-11-18 21:29:06 +0700

    add bodyParser

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+
 require("dotenv").config();
 const axios = require("axios");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
 const port = process.env.PORT || 5001;
 
 app.post("/gpa", async (req, res) => {
